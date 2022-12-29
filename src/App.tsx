59e46a52d70b77ce26cf563ebeebbfee4d7cd2d4
bryan vera <bveracachay@gmail.com>
2022-12-28 22:11:36 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/scenes/navbar";
+import { SelectedPage } from "@/shared/types";
+import { useState } from "react";
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  return (
+    <div className="App bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
